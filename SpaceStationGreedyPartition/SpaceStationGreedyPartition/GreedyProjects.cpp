49dedf38f0668f5856625363adccbe3915462609
Ese <eseinyang2005@hotmail.com>
2015-11-02 10:58:36 -0500

    Completed partition algorithm

--- SpaceStationGreedyPartition/SpaceStationGreedyPartition/GreedyProjects.cpp
+++ SpaceStationGreedyPartition/SpaceStationGreedyPartition/GreedyProjects.cpp
@@ -1,17 +1,37 @@
 // SpaceStationGreedyPartition.cpp : Defines the entry point for the console application.
-//
+//Name: Ese Okonofua SN: 10107285
 
-#include "stdafx.h"
+#include "stdafx.h" //Visual studio standard ignore
 #include "GreedyProjects.h"
 int main()
 {
-	ResearchPeriod period = ResearchPeriod("SpaceV2.txt");
-	vector<Project> projects = period.getProjects();
-	vector<Project> optimal = period.optimal();
-	for (auto& i : optimal)
-		cout << i;
-
-    return 0;
+	ofstream myfile;
+	myfile.open("Output.txt", ios::out);
+	if (myfile.fail())
+		cerr << "File can't be opened" << endl;
+	else if (myfile.is_open()) {
+		ResearchPeriod period = ResearchPeriod("SpaceStation.txt");
+		vector<Project> projects = period.getProjects();
+		vector<Project> optimal = period.getOptimalProjects();
+		myfile << "All the projects read from the file:" << endl;
+		for (auto& i : projects)
+			myfile << i;
+		myfile << endl;
+		myfile << "The selected optimal projects calculated:" << endl;
+		for (auto& i : optimal)
+			myfile << i;
+		myfile << endl;
+		myfile << "Group 1 Projects:" << endl;
+		for (auto& i : period.getPartition1())
+			myfile << i;
+		myfile << endl;
+		myfile << "Group 2 Projects:" << endl;
+		for (auto& i : period.getPartition2())
+			myfile << i;
+		myfile.close();
+	}
+	
+	return 0;
 }
 
 
@@ -38,21 +58,28 @@ Project getProject(string& line) {
 	return project;
 }
 
-Project::Project(const int & projectNumber, const int & startTime, const int & finishTime): projectNumber(projectNumber), startTime(startTime), finishTime(finishTime){}
+Project::Project(const int & projectNumber, const int & startTime, const int & finishTime) : projectNumber(projectNumber), startTime(startTime), finishTime(finishTime) {
+	projectCost = finishTime - startTime;
+}
 
-int Project::Si() const{return startTime;}
+int Project::Si() const { return startTime; }
 
-int Project::Fi() const{return finishTime;}
+int Project::Fi() const { return finishTime; }
 
-int Project::PN() const{return projectNumber;}
+int Project::PN() const { return projectNumber; }
+
+int Project::ProjectCost() const
+{
+	return projectCost;
+}
 
 ostream& operator<<(ostream& out, const Project& project) {
-	out << "Project Number: " << project.projectNumber << "| Start Time: " << project.startTime << "| Finish Time: " << project.finishTime << endl;
+	out << "Project Number: " << project.projectNumber << "| Start Time: " << project.startTime << "| Finish Time: " << project.finishTime << "| Project Cost: " << project.projectCost << endl;
 	return out;
 }
 
 
-ResearchPeriod::ResearchPeriod(const string & document):document(document)
+ResearchPeriod::ResearchPeriod(const string & document) :document(document)
 {
 	string line;
 	ifstream myfile(document);
@@ -68,56 +95,142 @@ ResearchPeriod::ResearchPeriod(const string & document):document(document)
 		while (getline(myfile, line)) {
 			projects.push_back(getProject(line));
 		}
-		projects = sortProjects(projects);
+		projects = sortProjectsFinishTime(projects);
 		myfile.close();
 	}
+	optimalProjects = findOptimal();
+	optimalProjects = sortProjectCost(optimalProjects);
+	partitionProjects();
 }
 
-vector<Project> ResearchPeriod::optimal()
+/*FindOptimal() function: Takes a list of projects and finds the Minimum amount of projects that will keep 
+the researchers busy at all time. The projects are sorted by Finish time in descending order. The function first decides
+what the last project to be picked is by picking the project with the least start time that has a finish time greater than
+or equal to the research end time. From then on it goes backwards selecting projects that have the least start time but have 
+a finish time >= the start time of the previous project.
+*/
+vector<Project> ResearchPeriod::findOptimal()
 {
 	vector<Project> optimalResults;
 	Project selectedProject = projects[0]; //Point to first project
-	for (auto& i : projects) {
+	for (auto& i : projects) { //Pick last project in the list
 		if (i.Fi() >= researchEndTime && i.Si() <= selectedProject.Si())
 			selectedProject = i;
 		else if (i.Fi() < researchEndTime) {
 			optimalResults.push_back(selectedProject);
 			break;
-		}	
+		}
 	}
 
-	for (int i= 0; selectedProject.Si() > reasearchStartTime ; i++) {
-		if (projects[i].Si() < selectedProject.Si() && projects[i].Fi() >= optimalResults.back().Si()) 
+	for (int i = 0; selectedProject.Si() > reasearchStartTime; i++) { //Iterate through picking values 
+		if (projects[i].Si() < selectedProject.Si() && projects[i].Fi() >= optimalResults.back().Si())
 			selectedProject = projects[i];
 		else if (projects[i].Fi() < optimalResults.back().Si()) {
 			optimalResults.push_back(selectedProject);
 			selectedProject = projects[i];
 		}
-		else if (projects[i].Si() <= reasearchStartTime) 
-			optimalResults.push_back(projects[i]);	
+		else if (projects[i].Si() <= reasearchStartTime)
+			optimalResults.push_back(projects[i]);
 	}
 	optimalResults.push_back(selectedProject);
 	return optimalResults;
 }
 
-vector<Project> ResearchPeriod::sortProjects(vector<Project>& projects)
+vector<Project> ResearchPeriod::sortProjectCost(vector<Project>& projects)
 {
 	if (projects.size() <= 1)
 		return projects;
 	else {
-		vector<Project> projectLeft(projects.begin(), projects.begin()+ projects.size() / 2);
+		vector<Project> projectLeft(projects.begin(), projects.begin() + projects.size() / 2);
 		vector<Project> projectRight(projects.begin() + projects.size() / 2, projects.end());
-		projectLeft = sortProjects(projectLeft);
-		projectRight = sortProjects(projectRight);
+		projectLeft = sortProjectCost(projectLeft);
+		projectRight = sortProjectCost(projectRight);
 
-		return merge(projectLeft, projectRight);
+		return mergeProjectCost(projectLeft, projectRight);
 	}
+}
 
+vector<Project> ResearchPeriod::mergeProjectCost(vector<Project>& leftProject, vector<Project>& rightProject)
+{
+	vector<Project> result;
+	while (leftProject.size() != 0 && rightProject.size() != 0) {
+		if (leftProject[0].ProjectCost() <= rightProject[0].ProjectCost()) {
+			result.push_back(leftProject[0]);
+			leftProject.erase(leftProject.begin());
+		}
+		else {
+			result.push_back(rightProject[0]);
+			rightProject.erase(rightProject.begin());
+		}
+	}
+	while (leftProject.size() != 0) {
+		result.push_back(leftProject[0]);
+		leftProject.erase(leftProject.begin());
+	}
+	while (rightProject.size() != 0) {
+		result.push_back(rightProject[0]);
+		rightProject.erase(rightProject.begin());
+	}
+	return result;
+}
+/*partitionProjects() function: the purpose of this algorithm is to take the list of optimal projects and find
+the split them into a list of 2 while minimizing their difference. The algorithm works by taking the initial list of
+optimal projects sorted in ascending order and moving values from the initial list into a new list which acts
+as the second partition. The while loops stops when the weight of the new list is greater than the initial list
+or if there are no more values that will reduce the difference between the 2.*/
+void ResearchPeriod::partitionProjects()
+{
+	vector<Project> projectList = sortProjectCost(optimalProjects);
+	vector<Project> newList; 
+	while (projectSum(newList) < projectSum(projectList)) {
+		int difference = abs(projectSum(projectList) - projectSum(newList));
+		int newDifference = abs(projectSum(projectList) - projectSum(newList));
+		auto removeValue = projectList.begin();
+		for (auto position = projectList.begin(); position != projectList.end(); ++position) {
+			if (abs((projectSum(projectList) - position->ProjectCost()) - (projectSum(newList) + position->ProjectCost())) < difference) {
+				difference = abs((projectSum(projectList) - position->ProjectCost()) - (projectSum(newList) + position->ProjectCost()));
+				removeValue = position;
+			}
+		}
+		if (difference < newDifference) {
+			newList.push_back(*removeValue);
+			projectList.erase(removeValue);
+		}
+		else
+			break;
+	
+	}
+	
+	partition1 = projectList;
+	partition2 = newList;
 	
 
 }
 
-vector<Project> ResearchPeriod::merge(vector<Project>& leftProject, vector<Project>& rightProject)
+int ResearchPeriod::projectSum(vector<Project>& list)
+{
+	int sum(0);
+	for (auto& i : list) {
+		sum += i.ProjectCost();
+	}
+	return sum;
+}
+
+vector<Project> ResearchPeriod::sortProjectsFinishTime(vector<Project>& projects)
+{
+	if (projects.size() <= 1)
+		return projects;
+	else {
+		vector<Project> projectLeft(projects.begin(), projects.begin() + projects.size() / 2);
+		vector<Project> projectRight(projects.begin() + projects.size() / 2, projects.end());
+		projectLeft = sortProjectsFinishTime(projectLeft);
+		projectRight = sortProjectsFinishTime(projectRight);
+
+		return mergeFinishTime(projectLeft, projectRight);
+	}
+}
+
+vector<Project> ResearchPeriod::mergeFinishTime(vector<Project>& leftProject, vector<Project>& rightProject)
 {
 	vector<Project> result;
 	while (leftProject.size() != 0 && rightProject.size() != 0) {
@@ -154,4 +267,19 @@ int ResearchPeriod::getResearchStartTime()
 int ResearchPeriod::getResearchEndTime()
 {
 	return researchEndTime;
+}
+
+vector<Project> ResearchPeriod::getOptimalProjects()
+{
+	return optimalProjects;
+}
+
+vector<Project> ResearchPeriod::getPartition1()
+{
+	return partition1;
+}
+
+vector<Project> ResearchPeriod::getPartition2()
+{
+	return partition2;
 }
--- SpaceStationGreedyPartition/SpaceStationGreedyPartition/GreedyProjects.h
+++ SpaceStationGreedyPartition/SpaceStationGreedyPartition/GreedyProjects.h
@@ -1,3 +1,4 @@
+//Name: Ese Okonofua SN: 10107285
 #pragma once
 #include <string>
 #include <vector>
@@ -11,28 +12,50 @@ class Project { //Project Object to hold start time, end time and project # attr
 	int startTime;
 	int finishTime; //Attributes of a project.
 	int projectNumber;
+	int projectCost;
 public:
 	Project(const int& projectNumber, const int& startTime, const int& finishTime);
 	int Si() const;
 	int Fi() const; //Getters for private attributes.
 	int PN() const;
+	int ProjectCost() const;
 	friend ostream& operator<<(ostream& out, const Project& project); //What printing a project does
 };
 
 class ResearchPeriod { //Object holding research specifications. e.g. # Of Projects,Research critical period, Optimal List of Projects 
 	string document;
-	vector<Project> projects;
-	vector<Project> sortProjects(vector<Project>& projects); //Sorts projects based on finish time in descending order
-	vector<Project> merge(vector<Project>& leftProject, vector<Project>& rightProject);
+	vector<Project> projects;//Projects that are read from the inputfile.
+
+	vector<Project> sortProjectsFinishTime(vector<Project>& projects); //Sorts projects based on finish time in descending order
+	vector<Project> mergeFinishTime(vector<Project>& leftProject, vector<Project>& rightProject);
+
+	vector<Project> findOptimal(); //Function to find optimal projects to use, Algorithm explained in .cpp file
+	vector<Project> optimalProjects;
+
+	vector<Project> sortProjectCost(vector<Project>& projects);
+	vector<Project> mergeProjectCost(vector<Project>& leftProject, vector<Project>& rightProject);
+
+	void partitionProjects(); //Function to partition the project objects Algorithm Explained in .Cpp file
+	int projectSum(vector<Project>& list); //Finds the sum in a list of projects
+	vector<Project> partition1;
+	vector<Project> partition2;
+
 	int reasearchStartTime;
 	int researchEndTime;
 	int numOfProjects;
+
 public:
-	ResearchPeriod(const string& document); //Constructor with document holding research specifications
-	vector<Project> optimal(); //Finds optimal projects to do 
-	vector<Project> getProjects();
+	ResearchPeriod(const string& document); //Constructor with document holding research specifications	
 	int getResearchStartTime();
 	int getResearchEndTime();
+
+	//Finds optimal projects to do 
+	vector<Project> getProjects();
+	vector<Project> getOptimalProjects();
+
+	//Partition Solutions
+	vector<Project> getPartition1();
+	vector<Project> getPartition2();
 
 };
 vector<int> getTimeLine(string& line);
